bf265beb495e57f2d3ff8846d916fdcc1a32eb49
rainofpain <guestnewbie013@gmail.com>
2025-11-18 19:50:21 +0200

    added mentor_id filter for join handler

--- modules/command_handlers/join_handler.py
+++ modules/command_handlers/join_handler.py
@@ -2,11 +2,16 @@ import aiogram.filters as filters
 import aiogram.types as types
 import aiogram.fsm.context as context
 
-from config import dispatcher
+from config import dispatcher, mentors_ids
 from ..state_groups import Join_state
 
 @dispatcher.message(filters.Command(commands = "join"))
 async def join_command(message: types.Message, state: context.FSMContext):
-    await message.answer(text = "Введіть код: ")
-    await state.set_state(Join_state.code)
+    telegram_id = message.from_user.id
+    
+    if telegram_id in mentors_ids:
+        await message.answer(text = "Ментор не може проходити тест")
+    else:
+        await message.answer(text = "Введіть код: ")
+        await state.set_state(Join_state.code)
     
